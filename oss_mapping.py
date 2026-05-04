90a1db0187cf8b1153f2bfc8d194ebf935754035
jeongwonhyeong38-ux <jeongwonhyeong38@gmail.com>
2026-05-04 17:07:23 +0900

    Add files via upload

---
+++ oss_mapping.py
@@ -0,0 +1,10 @@
+{
+  "열공": "hard study",
+  "행쇼": "be happy",
+  "가즈아": "let's go",
+  "킹받네": "so annoying",
+  "않되": "안 돼",
+  "내 일": "내일",
+  "졌잘싸": "good game",
+  "알잘딱깔센": "perfectly handled"
+}
